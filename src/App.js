f724dac359afab9707bebf7146d96165fe6f71fb
PremePanther <shpadza@gmail.com>
2019-07-03 20:01:29 -0400

    fixed background and header positioning

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Page from "./components/Page";
 {
   /* TO DO
   - reroute cards to store page
-  - make header look centered
-  - create ABOUT, PODCAST & CART
-      -route everything after
-  - make footer useful with contact N'ting
+
   - media queries are needed for polishing
       -header should hide left links and center logo
       with the right hand links
