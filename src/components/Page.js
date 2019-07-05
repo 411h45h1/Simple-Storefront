e55365151b6e7d56c574d44b7b9c9f596d129bb1
PremePanther <shpadza@gmail.com>
2019-07-05 11:51:30 -0400

    home page edit

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../App.css";
 import Header from "./Header";
+import HomePage from "./HomePage";
 
 class Page extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <HomePage />
       </div>
     );
   }
