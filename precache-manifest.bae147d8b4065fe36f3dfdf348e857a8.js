064a0aa687c4334ab7756aab8246fbbc55b379ee
PremePanther <shpadza@gmail.com>
2019-07-07 18:39:00 -0400

    Updates

---
+++ precache-manifest.bae147d8b4065fe36f3dfdf348e857a8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3c58e32bae9a5e369a550a19cc99e7",
+    "url": "/React-JS-Store/index.html"
+  },
+  {
+    "revision": "6a5fbf36c87972def5e4",
+    "url": "/React-JS-Store/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "abca5fa6d129b392f63a",
+    "url": "/React-JS-Store/static/css/main.aff28aab.chunk.css"
+  },
+  {
+    "revision": "6a5fbf36c87972def5e4",
+    "url": "/React-JS-Store/static/js/2.76b5f615.chunk.js"
+  },
+  {
+    "revision": "abca5fa6d129b392f63a",
+    "url": "/React-JS-Store/static/js/main.a916c6cd.chunk.js"
+  },
+  {
+    "revision": "ffbeaa792faed08d727a",
+    "url": "/React-JS-Store/static/js/runtime~main.7309c2ed.js"
+  },
+  {
+    "revision": "d8705991f48ea1321b03a44a0e38510a",
+    "url": "/React-JS-Store/static/media/Item3.d8705991.png"
+  },
+  {
+    "revision": "e2c569e621a05261a155d63ee0348860",
+    "url": "/React-JS-Store/static/media/Landscape.e2c569e6.jpg"
+  },
+  {
+    "revision": "0888aec824fd06ccd40d280f1d7ec9cf",
+    "url": "/React-JS-Store/static/media/Reflex.0888aec8.svg"
+  }
+]);
