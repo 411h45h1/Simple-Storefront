a40d909866db5a8f3130ce26830d61d0aaf90b52
PremePanther <shpadza@gmail.com>
2019-07-09 21:50:34 -0400

    Updates

---
+++ precache-manifest.89f0617a3e4f709842ee3b47e7d18121.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e289fe628259d355a8d830bf78dd4bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a5fbf36c87972def5e4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d754859af924d14d67e4",
+    "url": "/static/css/main.9554df04.chunk.css"
+  },
+  {
+    "revision": "6a5fbf36c87972def5e4",
+    "url": "/static/js/2.76b5f615.chunk.js"
+  },
+  {
+    "revision": "d754859af924d14d67e4",
+    "url": "/static/js/main.a48efb6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d8705991f48ea1321b03a44a0e38510a",
+    "url": "/static/media/Item3.d8705991.png"
+  },
+  {
+    "revision": "e2c569e621a05261a155d63ee0348860",
+    "url": "/static/media/Landscape.e2c569e6.jpg"
+  },
+  {
+    "revision": "0888aec824fd06ccd40d280f1d7ec9cf",
+    "url": "/static/media/Reflex.0888aec8.svg"
+  }
+]);
