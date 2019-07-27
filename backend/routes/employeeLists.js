9ad727ed0e6d7f6264cdf4d9cc9fbb6887596516
PremePanther <shpadza@gmail.com>
2019-07-26 22:42:38 -0400

    update functioning (C.R.U.D)

--- backend/routes/employeeLists.js
+++ backend/routes/employeeLists.js
@@ -22,4 +22,30 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Employee.findById(req.params.id)
+    .then(employeeLists => res.json(employeeLists))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Employee.findByIdAndDelete(req.prams.id)
+    .then(() => res.json("Employee deleted"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Employee.findById(req.params.id)
+    .then(employeeLists => {
+      employeeLists.username = req.body.username;
+      employeeLists.title = req.body.title;
+
+      employeeLists
+        .save()
+        .then(() => res.json("Employees updated!"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
